a7ebd5ee9dca340c1d0d5693153974e2428d06e6
B-HANSEN <b.hansen.ubiqum@gmail.com>
2020-11-21 22:38:46 +0100

    added logic for user authentication

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const SALT_I = 10;
 const Schema = mongoose.Schema;
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const userSchema = new Schema({
 	email: {
@@ -59,6 +61,24 @@ userSchema.pre('save', function (next) {
 		next();
 	}
 });
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+	bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+		if (err) return cb(err);
+		cb(null, isMatch);
+	});
+};
+
+userSchema.methods.generateToken = function (cb) {
+	var user = this;
+	var token = jwt.sign(user._id.toHexString(), process.env.SECRET); // mongo saves as user._id
+
+	user.token = token;
+	user.save(function (err, user) {
+		if (err) return cb(err);
+		cb(null, user);
+	});
+};
 
 const User = mongoose.model('User', userSchema);
 
